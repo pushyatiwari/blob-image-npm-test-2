202109237d20e4d3cb8bb246100f1c63000aadf4
Pushya Tiwari <pushya.tiwari@gmail.com>
2024-04-01 20:18:52 +0530

    test commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,14 @@
+import { Text, View } from 'react-native';
+import React from 'react';
+
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
+}
+
+export function BlobImage(styles: any) {
+  return (
+    <View style={styles}>
+      <Text>this is a test package</Text>
+    </View>
+  );
 }
